e902072e2b48d1e69a2f83b57d9b9f789cae5e89
hebaa <hebaafouaad@gmail.com>
2021-12-21 00:15:42 +0200

    Adding some dynamically in the code

--- src/test/java/steps/GetRequestSteps.java
+++ src/test/java/steps/GetRequestSteps.java
@@ -23,24 +23,25 @@ public class GetRequestSteps {
     }
 
     @Given("Color Lover Patterns API endpoint exists")
-    public void checkTheAvailability() {
+    public void Color_Lover_Patterns_API_endpoint_exists() {
         request = RestAssured.given().
                 header("User-Agent", "PostmanRuntime/7.28.4");
     }
 
     @When("I send a valid Get Request to Get Patterns API XML File")
-    public void setGetRequest() {
+    public void I_send_a_valid_Get_Request_to_Get_Patterns_API_XML_File() {
         response = request.get();
     }
 
     @Then("response status code should be {int}")
-    public void validateTheStatusCode(int code) {
+    public void response_status_code_should_be(int code) {
         Assert.assertEquals(response.getStatusCode(), code);
     }
 
     @And("I should see the Number of Views of Pattern {string} Greater Than {string}")
-    public void validateTheNumViews(String patternNum, String threshold) {
+    public void I_should_see_the_Number_of_Views_of_Patter(String patternNum, String threshold) {
         assertThat(response.then().extract().path("patterns.pattern[" + patternNum + "].numViews"),
                 greaterThan(threshold));
+        System.out.println(response.then().extract().path("patterns.pattern[" + patternNum + "].numViews").toString());
     }
 }
